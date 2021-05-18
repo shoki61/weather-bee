0887a8fc5a308a935639d6561a858903d5fbb18a
Sohrat Jumadurdyyev <sohrat6128@gmail.com>
2021-05-18 19:53:37 +0300

    create project

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import { View, Text, StatusBar } from 'react-native';
+
+
+const App = () => (
+  <Fragment>
+    <StatusBar backgroundColor='#000'/>
+    <Text>App.js</Text>
+  </Fragment>
+);
+
+export default App;
